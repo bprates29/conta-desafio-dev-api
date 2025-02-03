1827251d4b0be1b05b5871d14a652fa880819ab1
bernardo_prates <bernardo_prates@sicredi.com.br>
2025-02-03 13:41:11 -0300

    adicionando spring boot no projeto

---
+++ src/main/java/br/com/bprates/conta_desafio_dev_api/ContaDesafioDevApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bprates.conta_desafio_dev_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaDesafioDevApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaDesafioDevApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bprates/conta_desafio_dev_api/ContaDesafioDevApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bprates.conta_desafio_dev_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaDesafioDevApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
